41c68e5c3546194cbf5e0e631d04bbeb4ae75164
kevinkalvin62 <134903820+kevinkalvin62@users.noreply.github.com>
2023-08-29 23:27:23 -0600

    desarrollo de la pantalla de inicio de sesion

---
+++ hotelAlura/src/main/java/com/mycompany/hotelalura/HotelAlura.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hotelalura;
+import graficos.inicioSecion;
+
+/**
+ *
+ * @author kevin
+ */
+public class HotelAlura {
+
+    public static void main(String[] args) {
+        inicioSecion entrada= new inicioSecion();
+        entrada.setVisible(true);
+        
+    }
+}
---
+++ hotelAlura/src/main/java/com/mycompany/hotelalura/USUARIO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.hotelalura;
+
+/**
+ *
+ * @author kevin
+ */
+public class USUARIO {
+    private String ususario="kevin";
+    private String contraseña= "123456";
+    public String getUsuario(){return this.ususario;}
+    public String getContraseña(){return this.contraseña;}
+}
